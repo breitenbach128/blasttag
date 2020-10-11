67b49e4fb2931012416de3a44377b292c705f3ae
Ben Breitenbach <breitenbach128@hotmail.com>
2020-10-11 21:13:19 +0300

    Worked general structure

--- authserver/js/game.js
+++ authserver/js/game.js
@@ -38,7 +38,8 @@ const config = {
             x: Math.floor(Math.random() * 700) + 50,
             y: Math.floor(Math.random() * 500) + 50,
             playerId: socket.id,
-            team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
+            team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue',
+            input: [0,0] // X,Y vector
         };
         // add player to server
         addPlayer(self, players[socket.id]);
@@ -55,20 +56,50 @@ const config = {
             // emit a message to all players to remove this player
             io.emit('disconnect', socket.id);
         });
+        socket.on('playerInput', function (inputData) {
+            intData = convertArrayStringToInteger(inputData);
+            handlePlayerInput(self, socket.id, intData);
+        });
     });
 
   }
    
-  function update() {}
+  function update() {
+    this.players.getChildren().forEach((player) => {
+        const input = players[player.playerId].input;
+        let speed = 100;
+        player.setVelocity(input[0]*speed,input[1]*speed);
+        players[player.playerId].x = player.x;
+        players[player.playerId].y = player.y;
+        players[player.playerId].rotation = player.rotation;
+      });
+      this.physics.world.wrap(this.players, 5);
+      io.emit('playerUpdates', players);
+  }
 
+  function handlePlayerInput(self, playerId, input) {
+    self.players.getChildren().forEach((player) => {
+      if (playerId === player.playerId) {
+        players[player.playerId].input = input;
+        
+      }
+    });
+  }
+  function convertArrayStringToInteger(a){
+    var result = a.map(function (x) { 
+        return parseInt(x, 10); 
+      });
+    return result;
+  }
   function addPlayer(self, playerInfo) {
-    const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'player').setOrigin(0.5, 0.5).setDisplaySize(48, 48);
-    player.setDrag(100);
-    player.setAngularDrag(100);
+    const player = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'player').setOrigin(0.5, 0.5).setDisplaySize(48, 48);
+    // player.setDrag(100);
+    // player.setAngularDrag(100);
     player.setMaxVelocity(200);
     player.playerId = playerInfo.playerId;
     self.players.add(player);
   }
+
   function removePlayer(self, playerId) {
     self.players.getChildren().forEach((player) => {
       if (playerId === player.playerId) {
--- public/js/game.js
+++ public/js/game.js
@@ -2,7 +2,8 @@ var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
-    height: 600,    
+    height: 600,  
+    pixelArt: true,  
     physics: {
         default: 'arcade',
         arcade: {
@@ -27,7 +28,7 @@ function create() {
     var self = this;
     this.socket = io();
     this.players = this.physics.add.group();
-
+    this.controls = createControls(self);
     this.socket.on('currentPlayers', function (players) {
         Object.keys(players).forEach(function (id) {
             if (players[id].playerId === self.socket.id) {
@@ -49,14 +50,60 @@ function create() {
             }
         });
     });
+    
+    this.socket.on('playerUpdates', function (players) {
+        Object.keys(players).forEach(function (id) {
+            self.players.getChildren().forEach(function (player) {
+                if (players[id].playerId === player.playerId) {
+                    player.setRotation(players[id].rotation);
+                    player.setPosition(players[id].x, players[id].y);
+                }
+            });
+        });
+    });
 }
 
-function update() { }
+function update() { 
+    let moveStateReqUpdate = false;
+    let moveState =[0,0];
+    //Movement State update
+    //Up/Down Vector
+    if(this.controls.W.b.isDown){
+        moveStateReqUpdate = true;
+        moveState[1] -=1;
+    }
+    
+    if(this.controls.S.b.isDown){
+        moveStateReqUpdate = true;
+        moveState[1] += 1;
+    }
+    //Left/Right Vector
+    if(this.controls.A.b.isDown){
+        moveStateReqUpdate = true;
+        moveState[0]-= 1;
+    }
+    if(this.controls.D.b.isDown){
+        moveStateReqUpdate = true;
+        moveState[0]+= 1;
+    }
+    if(moveStateReqUpdate){
+        this.socket.emit('playerInput', moveState);
+    }
 
+}
 function displayPlayers(self, playerInfo, sprite) {
     const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(48, 48);
     // if (playerInfo.team === 'blue') player.setTint(0x0000ff);
     // else player.setTint(0xff0000);
     player.playerId = playerInfo.playerId;
     self.players.add(player);
+}
+function createControls(scene){
+    let controls = {
+        W: {b:scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),s:0},
+        S: {b:scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),s:0},
+        A: {b:scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),s:0},
+        D: {b:scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),s:0}
+    }
+    return controls;
 }
